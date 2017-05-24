668d45da969123eb4242999f04ee402f5f6a60ba
Максим Киселев <m.kiselev@mrcur.local>
2017-05-24 19:53:35 +0300

    model seems to be fully working +  fake-root introduced +  drag&drop support +  editing each of the Employee's fields +  removing and inserting new employees

--- employeestreemodel.cpp
+++ employeestreemodel.cpp
@@ -1,12 +1,18 @@
 #include "employeestreemodel.h"
 
+#include <QMimeData>
+#include <QDataStream>
+
+
+const QString EmployeesTreeModel::EMPLOYEE_MIME_TYPE_NAME("EmployeesMimeType");
 
 
 // ----------------------------------------------------------------
 EmployeesTreeModel::EmployeesTreeModel(QObject* parent /*= nullptr*/)
     : QAbstractItemModel(parent)
+    , _root(new EmployeeTreeItem())
 {
-
+    _root->employee->name = tr("[root_element]");
 }
 
 
@@ -14,9 +20,11 @@ EmployeesTreeModel::EmployeesTreeModel(QObject* parent /*= nullptr*/)
 // ----------------------------------------------------------------
 int EmployeesTreeModel::rowCount(const QModelIndex& parent) const
 {
-    auto parentItem = _itemByIndex(parent);
+    // parent invalid -> we have root element, row count is 1
+    if(!parent.isValid())
+        return 1;
 
-    return ((parentItem) ? (parentItem->subordinates.size()) : (_roots.size()));
+    return static_cast<EmployeeTreeItem*>(parent.internalPointer())->subordinates.size();
 }
 
 
@@ -24,7 +32,7 @@ int EmployeesTreeModel::rowCount(const QModelIndex& parent) const
 // ----------------------------------------------------------------
 int EmployeesTreeModel::columnCount(const QModelIndex& parent) const
 {
-    return 1;
+    return 5;
 }
 
 
@@ -35,10 +43,7 @@ QModelIndex EmployeesTreeModel::index(int row, int column, const QModelIndex& pa
     if(!hasIndex(row, column, parent))
         return QModelIndex();
 
-    auto parentItem = _itemByIndex(parent);
-
-
-    return createIndex(row, column, ((parentItem) ? (parentItem->subordinates.at(row).data()) : (_roots.at(row).data())) );
+    return createIndex(row, column,  ((parent.isValid()) ? (static_cast<EmployeeTreeItem*>(parent.internalPointer())->subordinates.at(row).data()) : (_root.data())));
 }
 
 
@@ -52,13 +57,16 @@ QModelIndex EmployeesTreeModel::parent(const QModelIndex& child) const
     auto currItem = static_cast<EmployeeTreeItem*>(child.internalPointer());
     auto chiefRef = currItem->chief.toStrongRef();
 
+    // if !chiefRef - child is root element, and it's parent index is invalid index
     if(!chiefRef)
         return QModelIndex();
 
     auto chiefsChiefRef = chiefRef->chief.toStrongRef();
 
+    // defining parent row to create index
     int parentRow = 0;
 
+    // if !chiefsChiefRef - we got root element and it's row is 0
     if(chiefsChiefRef)
     {
         for(int i = 0; i < chiefsChiefRef->subordinates.size(); i++)
@@ -82,7 +90,29 @@ Qt::ItemFlags EmployeesTreeModel::flags(const QModelIndex& index) const
     if(!index.isValid())
         return Qt::ItemIsEnabled;
 
-    return (Qt::ItemIsEnabled | Qt::ItemIsSelectable);
+    // Root is not editable or dragable, but drop is enabled
+    if(static_cast<EmployeeTreeItem*>(index.internalPointer()) == _root.data())
+        return (Qt::ItemIsEnabled | Qt::ItemIsSelectable | Qt::ItemIsDropEnabled);
+
+    return (Qt::ItemIsEnabled | Qt::ItemIsSelectable | Qt::ItemIsEditable | Qt::ItemIsDragEnabled | Qt::ItemIsDropEnabled);
+}
+
+
+
+// ----------------------------------------------------------------
+Qt::DropActions EmployeesTreeModel::supportedDropActions() const
+{
+    return Qt::MoveAction;
+}
+
+
+
+// ----------------------------------------------------------------
+QStringList EmployeesTreeModel::mimeTypes() const
+{
+    QStringList types;
+    types << EMPLOYEE_MIME_TYPE_NAME;
+    return types;
 }
 
 
@@ -95,9 +125,39 @@ QVariant EmployeesTreeModel::data(const QModelIndex& index, int role /*= Qt::Dis
 
     auto item = static_cast<EmployeeTreeItem*>(index.internalPointer());
 
-    if(role == Qt::DisplayRole)
+    if(role == Qt::DisplayRole || role == Qt::EditRole)
     {
-        return item->employee->name;
+        if(item == _root.data())
+        {
+            if(index.column() == (int)Column::Name)
+                return item->employee->name;
+            else
+                return QVariant();
+        }
+        else
+        {
+            switch(index.column())
+            {
+            case Column::Name:
+                return item->employee->name;
+                break;
+            case Column::Surname:
+                return item->employee->surname;
+                break;
+            case Column::Patronymic:
+                return item->employee->patronymic;
+                break;
+            case Column::Appointment:
+                return item->employee->appointment;
+                break;
+            case Column::Birthdate:
+                return item->employee->birthdate;
+                break;
+            default:
+                return QVariant();
+                break;
+            }
+        }
     }
 
     return QVariant();
@@ -106,9 +166,59 @@ QVariant EmployeesTreeModel::data(const QModelIndex& index, int role /*= Qt::Dis
 
 
 // ----------------------------------------------------------------
+QMimeData* EmployeesTreeModel::mimeData(const QModelIndexList& indexes) const
+{
+    if(indexes.isEmpty() || !indexes.at(0).isValid())
+        return nullptr;
+
+    auto elemAddr = (quintptr)(indexes.at(0).internalPointer());
+
+    QByteArray encodedData(QString::number(elemAddr).toUtf8());
+
+    auto mimeData = new QMimeData();
+    mimeData->setData(EMPLOYEE_MIME_TYPE_NAME, encodedData);
+    return mimeData;
+}
+
+
+
+// ----------------------------------------------------------------
 bool EmployeesTreeModel::setData(const QModelIndex& index, const QVariant& value, int role)
 {
-    return false;
+    if(!index.isValid())
+        return false;
+
+    auto item = static_cast<EmployeeTreeItem*>(index.internalPointer());
+
+    if(role == Qt::EditRole)
+    {
+        switch(index.column())
+        {
+        case Column::Name:
+            item->employee->name = value.toString();
+            break;
+        case Column::Surname:
+            item->employee->surname = value.toString();
+            break;
+        case Column::Patronymic:
+            item->employee->patronymic = value.toString();
+            break;
+        case Column::Appointment:
+            item->employee->appointment = value.toString();
+            break;
+        case Column::Birthdate:
+            item->employee->birthdate = value.toDate();
+            break;
+        default:
+            return false;
+            break;
+        }
+
+        emit dataChanged(index, index);
+        return true;
+    }
+
+    return true;
 }
 
 
@@ -116,7 +226,27 @@ bool EmployeesTreeModel::setData(const QModelIndex& index, const QVariant& value
 // ----------------------------------------------------------------
 bool EmployeesTreeModel::insertRows(int row, int count, const QModelIndex& parent)
 {
-    return false;
+    // It's not supposed to have any elements on fake root element level
+    if(!parent.isValid())
+        return false;
+
+    const int last = row + count - 1;
+
+    auto chiefItem = static_cast<EmployeeTreeItem*>(parent.internalPointer());
+    const auto chiefShared = _getSharedForItem(chiefItem);
+
+    beginInsertRows(parent, row, last);
+
+
+    for(int i = row; i <= last; i++)
+    {
+        chiefItem->subordinates.insert(i, QSharedPointer<EmployeeTreeItem>::create());
+        chiefItem->subordinates.at(i)->chief = chiefShared;
+    }
+
+    endInsertRows();
+
+    return true;
 }
 
 
@@ -124,7 +254,96 @@ bool EmployeesTreeModel::insertRows(int row, int count, const QModelIndex& paren
 // ----------------------------------------------------------------
 bool EmployeesTreeModel::removeRows(int row, int count, const QModelIndex& parent)
 {
+    // It's not supposed you can delete fake root
+    if(!parent.isValid())
+        return false;
+
+    const int last = row + count - 1;
+
+    auto chiefItem = static_cast<EmployeeTreeItem*>(parent.internalPointer());
+
+    beginRemoveRows(parent, row, last);
+
+    for(int i = last; i <= row; i++)
+    {
+        chiefItem->subordinates.takeAt(i);
+    }
+
+    endRemoveRows();
+
+    return true;
+}
+
+
+
+// ----------------------------------------------------------------
+bool EmployeesTreeModel::moveRows(const QModelIndex& sourceParent, int sourceRow, int count, const QModelIndex& destinationParent, int destinationChild)
+{
+    // It's not supposed to move any elements on one level with fake-root or move fake-root itself
+    if(!destinationParent.isValid() || !sourceParent.isValid())
+        return false;
+
+    const int lastSourceR = sourceRow + count - 1;
+
+
+    const auto sourceParentItem = static_cast<EmployeeTreeItem*>(sourceParent.internalPointer());
+    const auto destinationParentItem = static_cast<EmployeeTreeItem*>(destinationParent.internalPointer());
+    const auto destinationParentShared = _getSharedForItem(destinationParentItem);
+
+    beginMoveRows(sourceParent, sourceRow, lastSourceR, destinationParent, destinationChild);
+
+    for(int i = destinationChild + 1; i <= destinationChild + count; i++)
+    {
+        auto pointerToMove = sourceParentItem->subordinates.takeAt(sourceRow);
+        pointerToMove->chief = destinationParentShared;
+        destinationParentItem->subordinates.insert(i, pointerToMove);
+    }
+
+    endMoveRows();
+
+    return true;
+}
+
+
+
+// ----------------------------------------------------------------
+bool EmployeesTreeModel::dropMimeData(const QMimeData* data, Qt::DropAction action, int row, int column, const QModelIndex& parent)
+{
+    if(action == Qt::IgnoreAction || !data)
+        return false;
+
+    if(!data->hasFormat(EMPLOYEE_MIME_TYPE_NAME))
+        return false;
+
+    if(!parent.isValid())
+        return false;
+
+    QByteArray encodedData = data->data(EMPLOYEE_MIME_TYPE_NAME);
+    auto elemAddr = (EmployeeTreeItem*)encodedData.toULongLong();
+    if(!elemAddr)
+        return false;
+    if(elemAddr == _root.data())
+        return false;
+
+    auto oldChiefIndex = _getElementIndex(elemAddr->chief.toStrongRef().data());
+    auto newChiefIndex = _getElementIndex(static_cast<EmployeeTreeItem*>(parent.internalPointer()));
+    // if oldChiefIndex is the same as newChiefIndex, don't need to do anything
+    if(oldChiefIndex == newChiefIndex)
+        return false;
+    auto oldElementsIndex = _getElementIndex(elemAddr);
+
+    moveRows(oldChiefIndex, oldElementsIndex.row(), 1, newChiefIndex, 0);
+
+    // returning false to prevent deletion of row, that was transferred
     return false;
+}
+
+
+
+// ----------------------------------------------------------------
+const QList<QSharedPointer<EmployeeTreeItem>> EmployeesTreeModel::getRoots() const
+{
+    return _root->subordinates;
 }
 
 
@@ -133,17 +352,67 @@ bool EmployeesTreeModel::removeRows(int row, int count, const QModelIndex& paren
 void EmployeesTreeModel::setRoots(const QList<QSharedPointer<EmployeeTreeItem> >& roots)
 {
     beginResetModel();
-    _roots = roots;
+
+    _root->subordinates.clear();
+    _root->subordinates.append(roots);
+
+    for(auto subroot : _root->subordinates)
+    {
+        subroot->chief = _root;
+    }
+
     endResetModel();
 }
 
 
 
 // ----------------------------------------------------------------
-EmployeeTreeItem* EmployeesTreeModel::_itemByIndex(const QModelIndex& index) const
+QModelIndex EmployeesTreeModel::_getElementIndex(EmployeeTreeItem* item)
 {
-    if(!index.isValid())
-        return nullptr;
+    if(!item)
+        return QModelIndex();
 
-    return static_cast<EmployeeTreeItem*>(index.internalPointer());
+    auto chiefRef = item->chief.toStrongRef();
+    if(chiefRef)
+    {
+        for(int i = 0; i < chiefRef->subordinates.size(); i++)
+        {
+            if(chiefRef->subordinates.at(i).data() == item)
+                return createIndex(i, 0, item);
+        }
+    }
+    else
+    {
+        // we have root element
+        return createIndex(0, 0, item);
+    }
+
+    return QModelIndex();
+}
+
+
+
+// ----------------------------------------------------------------
+QSharedPointer<EmployeeTreeItem> EmployeesTreeModel::_getSharedForItem(EmployeeTreeItem* item)
+{
+    if(!item)
+        return QSharedPointer<EmployeeTreeItem>();
+
+    auto chiefRef = item->chief.toStrongRef();
+
+    if(chiefRef)
+    {
+        for(int i = 0; i < chiefRef->subordinates.size(); i++)
+        {
+            if(chiefRef->subordinates.at(i).data() == item)
+                return QSharedPointer<EmployeeTreeItem>(chiefRef->subordinates.at(i));
+        }
+    }
+    else
+    {
+        // we got root element
+        return QSharedPointer<EmployeeTreeItem>(_root);
+    }
+
+    return QSharedPointer<EmployeeTreeItem>();
 }
--- employeestreemodel.h
+++ employeestreemodel.h
@@ -6,7 +6,14 @@
 #include "employeeitem.h"
 
 /**
- * @brief The EmployeesTreeModel class - EMPTY REALIZATION ATM!
+ * @brief The EmployeesTreeModel class - used for representing EmployeeTreeItems' hierarchy.
+ * In model indexes (QModelIndex) in internal pointers are stored raw pointers to EmployeesTreeItems.
+ * This is normal, because all QModelIndexes are regenerated after any operations, changing model.
+ *
+ * All new roots, passed to the model are stored under fake-root vertex. This is used for proper drag&drop or
+ * insert operations. Fake root can't be deleted, dragged or no items can be inserted at it's level.
+ *
+ * @warning Drag&Drop support is made only for usage inside this model!
  */
 
 class EmployeesTreeModel : public QAbstractItemModel
@@ -14,6 +21,15 @@ class EmployeesTreeModel : public QAbstractItemModel
     Q_OBJECT
 
 public:
+    enum class Column
+    {
+        Name = 0,
+        Surname,
+        Patronymic,
+        Appointment,
+        Birthdate
+    };
+
     EmployeesTreeModel(QObject* parent = nullptr);
 
     // Implementations of QAbstractItemModel's class functions
@@ -24,21 +40,52 @@ public:
     QModelIndex parent(const QModelIndex& child) const override;
 
     Qt::ItemFlags flags(const QModelIndex& index) const override;
+    Qt::DropActions supportedDropActions() const override;
+    QStringList mimeTypes() const override;
     QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const override;
+    QMimeData* mimeData(const QModelIndexList& indexes) const override;
 
     bool setData(const QModelIndex &index, const QVariant &value, int role = Qt::EditRole) override;
 
     bool insertRows(int row, int count, const QModelIndex& parent) override;
     bool removeRows(int row, int count, const QModelIndex& parent) override;
+    bool moveRows(const QModelIndex &sourceParent, int sourceRow, int count, const QModelIndex &destinationParent, int destinationChild) override;
+    bool dropMimeData(const QMimeData *data, Qt::DropAction action, int row, int column, const QModelIndex &parent) override;
 
     // Custom functions
+    /**
+     * @brief getRoots - returning list of all real roots (items, underlying the fake-root element).
+     * Mainly should be used for deserializing model.
+     *
+     * @return
+     */
+    const QList<QSharedPointer<EmployeeTreeItem>> getRoots() const;
+
+    /**
+     * @brief setRoots - setting new roots. All current roots will be dropped.
+     * @param roots
+     */
     void setRoots(const QList<QSharedPointer<EmployeeTreeItem>>& roots);
 
 private:
-    QList<QSharedPointer<EmployeeTreeItem>> _roots;
+    QSharedPointer<EmployeeTreeItem> _root;
 
-    EmployeeTreeItem* _itemByIndex(const QModelIndex& index) const;
-//    int _rowForItem(EmployeeTreeItem* item, Employee)
+    /**
+     * @brief _getElementIndex - used for retrieving QModelIndex for provided EmployeeTreeItem
+     * @param item - supposed not to be nullptr, but if so, will return QModelIndex()
+     * @return
+     */
+    QModelIndex _getElementIndex(EmployeeTreeItem* item);
+
+    /**
+     * @brief _getSharedForItem - used for retrieving QSharedPointer, that originall manages provided EmployeeTreeItem
+     * raw pointer
+     * @param item
+     * @return empty QSharedPointer, if passed EmployeeTreeItem is nullptr;
+     */
+    QSharedPointer<EmployeeTreeItem> _getSharedForItem(EmployeeTreeItem* item);
+
+    static const QString EMPLOYEE_MIME_TYPE_NAME;
 };
 
 #endif // EMPLOYEESTREEMODEL_H
